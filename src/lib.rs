b2aef654b3ee86408f510ff7721bb8b384134459
epwalsh <epwalsh10@gmail.com>
2019-12-16 15:57:17 -0800

    updates to docs

--- src/lib.rs
+++ src/lib.rs
@@ -207,21 +207,38 @@ fn meta_path(resource_path: &PathBuf) -> PathBuf {
     meta_path
 }
 
+/// Holds information about a cached resource.
 #[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Meta {
-    resource: String,
-    etag: Option<String>,
+    pub resource: String,
+    pub etag: Option<String>,
 }
 
 impl Meta {
-    pub async fn to_file(&self, resource_path: &PathBuf) -> Result<(), Box<dyn error::Error>> {
+    async fn to_file(&self, resource_path: &PathBuf) -> Result<(), Box<dyn error::Error>> {
         let meta_path = meta_path(resource_path);
         let serialized = serde_json::to_string(self).unwrap();
         fs::write(meta_path, &serialized[..]).await?;
         Ok(())
     }
 
-    pub async fn from_file(resource_path: &PathBuf) -> Result<Self, Box<dyn error::Error>> {
+    /// Get the `Meta` from a cached resource.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use cached_path::{cached_path, Meta};
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// let resource = "https://github.com/epwalsh/rust-cached-path/blob/master/README.md";
+    /// let path = cached_path(resource).await?;
+    /// let meta = Meta::from_cache(&path).await?;
+    /// assert_eq!(&meta.resource[..], resource);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub async fn from_cache(resource_path: &PathBuf) -> Result<Self, Box<dyn error::Error>> {
         let meta_path = meta_path(resource_path);
         let serialized = fs::read_to_string(meta_path).await?;
         let meta: Meta = serde_json::from_str(&serialized[..]).unwrap();
