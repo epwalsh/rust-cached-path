81deeb3f81ecaff6d15c09e097767b6d8f0fd5be
epwalsh <epwalsh10@gmail.com>
2020-09-12 11:44:46 -0700

    test with actual file

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,8 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
+use std::fs;
+use std::io::Read;
+use std::path::PathBuf;
 use std::process::Command; // Run programs
 
 #[test]
@@ -10,6 +13,34 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("file does not exist"));
+
+    Ok(())
+}
+
+#[test]
+fn test_remote_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("cached-path")?;
+
+    cmd.arg("https://raw.githubusercontent.com/epwalsh/rust-cached-path/master/test_fixtures/utf-8_sample/utf-8_sample.txt");
+    let result = cmd.assert().success();
+    let output = result.get_output();
+    let mut stdout = String::from_utf8(output.stdout.clone()).unwrap();
+    // remove newline at the end.
+    stdout.pop();
+    let path = PathBuf::from(stdout);
+    println!("{:?}", path);
+    assert!(path.is_file());
+
+    // Ensure cached version exactly matches local version.
+    let cached_file = fs::File::open(&path)?;
+    let cached_bytes: Result<Vec<u8>, _> = cached_file.bytes().collect();
+    let cached_bytes = cached_bytes.unwrap();
+
+    let local_file = fs::File::open("test_fixtures/utf-8_sample/utf-8_sample.txt")?;
+    let local_bytes: Result<Vec<u8>, _> = local_file.bytes().collect();
+    let local_bytes = local_bytes.unwrap();
+
+    assert!(cached_bytes == local_bytes);
 
     Ok(())
 }
